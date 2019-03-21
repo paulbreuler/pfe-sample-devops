9ba67632a22a25a0e44014e6c164923397dc7249
Paul Breuler <paul.jbre@gmail.com>
2019-03-21 13:28:55 -0400

    fix merge conflicts

---
+++ src/WebResources/webpack.config.js
@@ -0,0 +1,33 @@
+﻿const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    optimization: {
+        minimizer: [new UglifyJsPlugin({
+            cache: './src/common/cache/index.ts',
+        })]
+    },
+    entry: {
+        Common: './src/common/index.ts',
+        Forms: './src/Forms/index.ts',
+        Ribbons: './src/Ribbons/index.ts'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        libraryTarget: 'umd',
+        library: ['PFE', '[name]'],
+        filename: 'pfe_[name].min.js',        
+        path: path.resolve(__dirname, 'dist')
+    }
+};
